abf76ac7e5da594212d75e0c07fb5e56327b798f
KYASODEE <105626341+KYASODEE@users.noreply.github.com>
2022-05-31 16:43:30 +0530

    Add files via upload

---
+++ StreamApi/StreamAPI3/NewsId.java
@@ -0,0 +1,27 @@
+package StreamAPI3;
+
+public class NewsId {
+
+	
+		private int newsId;
+		private String postedByUser;
+		private String commentByUser;
+		private String comment;
+	public NewsId(int newsId, String postedByUser, String commentByUser, String comment) {
+		this.newsId = newsId;
+		this.postedByUser = postedByUser;
+		this.commentByUser = commentByUser;
+		this.comment = comment;
+		} public int getNewsId() {
+		return newsId;
+		} public String getPostedByUser() {
+		return postedByUser;
+		} public String getCommentByUser() {
+		return commentByUser;
+		} public String getComment() {
+		return comment; 
+		}
+}
+		
+
+
