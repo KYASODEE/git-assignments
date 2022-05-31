41f83b3cfa25022489891dba1b49df862ed47556
KYASODEE <105626341+KYASODEE@users.noreply.github.com>
2022-05-31 12:10:35 +0530

    Add files via upload

---
+++ Java Datastructures/Assignment1/IncomeTax.java
@@ -0,0 +1,39 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class IncomeTax {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{
+			// int x;
+			float r,x;
+			Scanner s= new Scanner(System.in);
+			System.out.println("Enter income");
+			x=s.nextInt();
+
+			if(x>500000)
+			{
+			r=x*(0.3f);
+			}
+			else if(x>300000)
+			{
+			r=x*(0.2f);
+
+			}
+			else if(x>180000)
+			{
+			r=x*(0.1f);
+			}
+			else
+			{
+			r=x;
+			}
+			System.out.println(r);
+			}
+			}
+
+	}
+
+
